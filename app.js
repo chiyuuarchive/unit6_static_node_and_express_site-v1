ddc22c585deccd68000d15f58d988f5f17a3bdfc
chiyuuarchive <85938105+chiyuuarchive@users.noreply.github.com>
2021-08-05 13:32:44 +0200

    Ready for review

--- app.js
+++ app.js
@@ -3,25 +3,96 @@
 */
 // Import the Express module
 const express = require("express");
+const { render } = require("pug");
 //Create an Express application
 const app = express()
-// Set up view engine
-app.set("views engine", "pug");
+// Require project data from "data.json"
+const { projects } = require("./data.json");
+// Require personal data from "data.json"
+const { personal } = require("./data.json");
 
 
 /*
 * Adding Middlewares
 */
+// Set up a template engine to transform pug/jade templates into HTML files
+app.set("view engine", "pug");
 // Use the built-in json parser in Express
 app.use(express.json());
 // Use the built-in urlencoded payload parser in Express to parse with querystring library
 app.use(express.urlencoded({extended: false}));
+// Serve the static files
+app.use("/static", express.static("public"));
 
 
+/*
+* Rendering pug templates (Handling of "GET" requests from client)
+*/
+// Render the "home" page
+app.get("/", (req, res) => {
+    res.render("index", {projects});
+});
+
+// Render the "about" page
+app.get("/about", (req, res) => {
+    res.render("about", {personal});
+});
+
+// Render the "error" page
+app.get("/error", (req, res) => {
+    // Log out custom error handler indication
+    console.log('Custom error route called');
+
+    // Constrouct a new error object
+    const err = new Error();
+    err.message = "Internal Server Error"
+    err.status = 500;
+    res.render("error", {err})
+});
+
+// Render the "project" page
+app.get("/project/:id", (req, res, next) => {
+    const projectNum = +req.params.id;
+
+    if(projects[projectNum]) {
+        console.log("A project object is being passed to the project.pug");
+        const project = projects[projectNum];
+        res.render("project", {project});
+    }
+    else {
+        console.log("There is no project with this id");
+        const err = new Error();
+        err.status = 404;
+        err.message = "Page not found"
+        next(err);
+    }
+});
 
 
+/*
+* Error handling
+*/
+// Render the "page-not-found" page (404 Error handling)
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.render("page-not-found", {err});
+})
 
-// Assign a port for the express server
+// Redirect to index page if server receives "undefined" URL links. If "noroute" is requested, render the "page-not-found" template.
+app.get("/:id", (req, res, next) => {
+    const input = req.params.id;
+    if(/.+/.test(input)) {
+        const err = new Error();
+        err.status = 404;
+        err.message = "Page not found"
+        res.render("page-not-found", {err});
+    }
+})
+
+
+/*
+* Assign a port for the Express server
+*/
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
-  })
+  });
